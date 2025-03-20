71e7a290fe00c7a08cb773878f066e0ec9d33f80
mattiacoli <mattia.coli@aol.com>
2025-03-20 16:09:14 +0100

    edit Header with logo and navigation bar and add style to it

--- src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <h2>Footer</h2>
-    </footer>
-  )
-}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
+import logo from '/img/dc-logo.png'
+
 export default function Header() {
   return (
     <header className="site_header">
-      <h1>Header</h1>
+
+      <div className='container'>
+        {/* Logo */}
+        <img src={logo} alt="" />
+        {/* Navbar */}
+        <nav>
+          <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEOS</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+          </ul>
+        </nav>
+
+      </div>
+
     </header>
   )
 }
