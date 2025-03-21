c86bbdb600d3dd16b830f2f249bf228c9860001d
mattiacoli <mattia.coli@aol.com>
2025-03-21 15:24:55 +0100

    edit Card component: wrap image in figure; update CSS for card layout and load more button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,12 @@ export default function Card({ image, title }) {
   return (
     <div className="col-2">
       <div className="card">
-        <img src={image} alt={title} />
-        <div>
+
+        <figure>
+          <img src={image} alt={title} />
+        </figure>
+
+        <div className="title">
           {title}
         </div>
       </div>
