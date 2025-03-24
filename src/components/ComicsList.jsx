8f2cc41ffc1c5f0c9d532193339808ddca3c027c
mattiacoli <mattia.coli@aol.com>
2025-03-24 17:38:38 +0100

    add genre filter and dropdown to ComicsList component for enhanced search functionality

--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -5,11 +5,23 @@ export default function ComicsList({ comics }) {
 
   const [searchQuery, setSearchQuery] = useState('')
 
+  const [selectedGenre, setSelectedGenre] = useState('')
+
+  const handleGenreChange = (e) => {
+    setSelectedGenre(e.target.value)
+  }
+
+
   const filteredComics = comics.filter(comic => {
     return comic.title.toLowerCase().includes(searchQuery.toLowerCase())
-  }
-  )
-
+  })
+    .filter(comic => {
+      if (selectedGenre === '') {
+        return true
+      }
+      return comic.type === selectedGenre
+    }
+    )
 
   return (
     <div className="container">
@@ -17,17 +29,21 @@ export default function ComicsList({ comics }) {
       <div className="label">
         <h2>CURRENT SERIES</h2>
         <input type="text" placeholder="search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
-
-
-
+        <select name="select" id="" onChange={handleGenreChange}>
+          <option value="">All</option>
+          <option value="comic book">comic book</option>
+          <option value="graphic novel">graphic Novel</option>
+        </select>
       </div>
 
       <div className="row">
+
         {
           filteredComics.map(comic => (
             <Card key={comic.id} image={comic.thumb} title={comic.title} />
           ))
         }
+
       </div>
 
       <div className="loadMore">
